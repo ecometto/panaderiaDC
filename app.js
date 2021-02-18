0e8486de643d45063e9320cbea59a2934b681be9
ecometto <79264241+ecometto@users.noreply.github.com>
2021-02-18 10:44:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+var nombres = {
+	"nombre": 'JUAN', 
+	"apellido" : "gimenez",
+	"nacionalidad" : "argentino"}
+
+console.table(nombres)
+
+drawbox(blue);
+
+// //class Celular{
+// 	constructor (color, peso, pantala, camara, ram){
+// 		this.color = color
+// 		this.peso = peso
+// 		this.pantalla = pantala
+// 		this.camara = camara
+// 		this.ram =ram
+// 	}
+
+// 	info(){
+// 		return `
+// 		color: ${this.color} <br>
+// 		peso: ${this.peso} <br>
+// 		pantalla: ${this.pantalla} <br>
+// 		camara: ${this.camara} <br>
+// 		RAM: ${this.ram} <br>
+// 		` 
+// 	}
+
+
+// 	prenderApagar(){
+// 		if ( this.apagado == true){
+// 			alert("encendiendo su equipo")
+// 			apagado = False
+// 		}
+// 		else if (this.apagado == false) {
+// 			alert("apagando su equipo")
+// 			apagado = True
+// 		}
+	
+// 		}
+// }
+
+
+// samsungA31 = new Celular('rojo', 150, 6, '20mp', '4gb')
+// samsungA50 = new Celular('negro', 160, 6.5, '25mp', '4gb')
+// samsungA70 = new Celular('azul', 158, 6.2, '30mp', '6gb')
+
+// document.write(samsungA50.info())
+
+
+
+
+
